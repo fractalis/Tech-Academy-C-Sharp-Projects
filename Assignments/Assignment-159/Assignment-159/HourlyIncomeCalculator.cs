801bc810f5995d000c5a803b9d9b6c5af7fb7307
Scott Rallya <srallya@gmail.com>
2020-11-24 14:39:12 -0500

    Submission for Assignment 159

---
+++ Assignments/Assignment-159/Assignment-159/HourlyIncomeCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_159
+{
+    struct PayInfo
+    {
+        public PayInfo( int HourlyRate, int HoursWorkedPerWeek)
+        {
+            this.HourlyRate = HourlyRate;
+            this.HoursWorkedPerWeek = HoursWorkedPerWeek;
+        }
+
+        public int HourlyRate { get; set; }
+        public int HoursWorkedPerWeek { get; set; }
+
+        public int AnnualSalary
+        {
+            get
+            {
+                return (HourlyRate * HoursWorkedPerWeek) * 52;
+            }
+        }
+    }
+
+    class HourlyIncomeCalculator
+    {
+        public List<PayInfo> payInfos { get; set; }
+
+        public HourlyIncomeCalculator()
+        {
+            payInfos = new List<PayInfo>();
+        }
+
+        /// <summary>
+        /// Adds a new PayInfo record to our list of PayInfos
+        /// </summary>
+        public void AddPayInfo()
+        {
+            PayInfo payInfo = new PayInfo();
+
+            Console.WriteLine("Hourly Rate?");
+            payInfo.HourlyRate = GetUserInputAsInteger();
+
+            Console.WriteLine("Hours worked per week?");
+            payInfo.HoursWorkedPerWeek = GetUserInputAsInteger();
+
+            payInfos.Add(payInfo);
+        }
+
+        public IEnumerable<int> GetAnnualSalaries()
+        {
+            return payInfos.Select(x => x.AnnualSalary);
+        }
+
+        public bool DoesPersonXMakeMoreThanPersonY(int x, int y)
+        {
+            x -= 1;
+            y -= 1;
+
+            // Return false if either of the values are invalid (e.g, beyond the payInfos.count)
+            if ( x > payInfos.Count || x < 0 || y < 0 || y > payInfos.Count) { return false; }
+
+            return payInfos[x].AnnualSalary > payInfos[y].AnnualSalary;
+        }
+
+        private int GetUserInputAsInteger()
+        {
+            while(true)
+            {
+                try
+                {
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("Invalid value, please enter a valid number: ");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Assignments/Assignment-159/Assignment-159/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            HourlyIncomeCalculator hourlyIncomeCalc = new HourlyIncomeCalculator();
+
+            for(int i = 0; i < 2; i++) {
+                // Which Person Are we dealing with?
+                Console.WriteLine("Person " + (i + 1).ToString());
+                // Add Pay info
+                hourlyIncomeCalc.AddPayInfo();
+            }
+
+            // Get Annual salaries
+            var salaries = hourlyIncomeCalc.GetAnnualSalaries();
+
+            // Create anonymous type that has value in Value and index in Index.
+            foreach(var payInfo in salaries.Select((x, i) => new { Value = x, Index = i }))
+            {
+                // Display Annual Salary of the Person
+                Console.WriteLine("Annual salary of Person " + (payInfo.Index + 1).ToString());
+                Console.WriteLine($"{payInfo.Value:N0}");
+            }
+
+            // Query if Person 1 makes More than person 2
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(hourlyIncomeCalc.DoesPersonXMakeMoreThanPersonY(1, 2).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
