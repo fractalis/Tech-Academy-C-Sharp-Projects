d11b463f5f7d03e3acc77a67f892814531e40eab
Scott Rallya <srallya@gmail.com>
2020-11-24 09:23:09 -0500

    Add Welcome Screen
    Displays welcome screen and waits for user input before closing the program. Adds some color to give some style to the program.

---
+++ Assignments/Assignment-124/Assignment-124/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the Console Foreground and Background Color
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Print out welcome screen
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+
+            // Wait for user input so we can see the message
+            Console.Read();
+        }
+    }
+}
