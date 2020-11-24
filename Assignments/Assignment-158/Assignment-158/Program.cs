97eb5fe6ca0db39905e75e60511e245f8a7aaef3
Scott Rallya <srallya@gmail.com>
2020-11-24 13:20:01 -0500

    Submission for Assignment 158, which is an essentially a simple calculator.

---
+++ Assignments/Assignment-158/Assignment-158/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_158
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+            // Run the Simple Calculator
+            simpleCalculator.Run();
+
+            // Wait for user input before ending program
+            Console.ReadLine();
+        }
+    }
+}
