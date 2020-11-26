2b55a7c1e61d802d0525d73d76c732388018ec06
Scott Rallya <srallya@gmail.com>
2020-11-26 05:49:30 -0500

    Submission for Assignment 228

---
+++ Assignments/Assignment-231/Assignment-231/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        /// <summary>
+        /// Divides the given number by 2 and outputs the result to the console
+        /// </summary>
+        /// <param name="x">The number we want to divide two by</param>
+        public void DivideByTwo(int x)
+        {
+            int dividedByTwo = x / 2;
+            Console.WriteLine($"{x} divided by 2 equals = {dividedByTwo}");
+        }
+
+        /// <summary>
+        /// Multiplies the first parameter by the second parameter, storing the result in the output variable `result`
+        /// </summary>
+        /// <param name="x">The number we want to multiply</param>
+        /// <param name="y">The number we want to multiply by</param>
+        /// <param name="result">The result of the division</param>
+        public void MultiplyBy(int x, int y, out int result)
+        {
+            result = x * y;
+        }
+
+        /// <summary>
+        /// Multiplies the first parameter by the second parameter, storing the result in the output variable `result`
+        /// </summary>
+        /// <param name="x">The number we want to multiply</param>
+        /// <param name="y">The number we want to multiply by</param>
+        /// <param name="result">The result of the multiplication</param>
+        public void MultiplyBy(double x, double y, out double result)
+        {
+            result = x * y;
+        }
+    }
+}
---
+++ Assignments/Assignment-231/Assignment-231/UnitConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_231
+{
+    public static class UnitConverter
+    {
+        // Declare a dictionary of conversions, from string pairs to Func<double, double>
+        public static Dictionary<(String, String), Func<double, double>> Conversions = new Dictionary<(String, String), Func<double, double>>()
+        {
+            {("M", "CM"), (double x) => x * 100 },
+            {("CM", "M"), (double x) => x / 100.0 }
+        };
+
+        /// <summary>
+        /// Converts the value from the specified unit to the specified unit.
+        /// </summary>
+        /// <param name="from">The unit we want to convert from</param>
+        /// <param name="to">The unit we want to convert to</param>
+        /// <param name="value">The value we want to convert</param>
+        /// <returns></returns>
+        public static double ConvertFromUnitTo(string from, string to, double value)
+        {
+            if (Conversions.ContainsKey((from, to)))
+            {
+                return Conversions[(from, to)](value);
+            } 
+            else
+            {
+                return 0.0;
+            }
+        }
+
+    }
+}
