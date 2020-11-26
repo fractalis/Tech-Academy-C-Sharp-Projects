3e9f26c037bc22251140af742705c36099555740
Scott Rallya <srallya@gmail.com>
2020-11-25 20:59:03 -0500

    Submission for Assignment 228

---
+++ Assignments/Assignment-228/Assignment-228/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathUtilities mathUtilities = new MathUtilities();
+            Random random = new Random();
+
+            // Step 1.3 Call the method in the class, passing in two numbers
+            mathUtilities.CalculateFactorial(random.Next(100), random.Next(100));
+
+            // Step 1.4 Call the method in the class, specifying the parameters by name
+            mathUtilities.CalculateFactorial(x: random.Next(100), y: random.Next(100));
+            Console.ReadLine();
+        }
+    }
+}
