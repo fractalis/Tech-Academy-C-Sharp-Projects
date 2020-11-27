b46d343c79bbb5a5e4d17ddc0966aba19d9e5dc3
Scott Rallya <srallya@gmail.com>
2020-11-27 07:46:00 -0500

    Submission for Assignment 252

---
+++ Assignments/Assignment-252/Assignment-252/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_252
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Sample", "Student", 1);
+            Employee employee2 = new Employee("Sample", "Student", 1);
+
+            Console.WriteLine($"Employee 1 and Employee 2 are equal: {employee1 == employee2}");
+
+            Employee employee3 = new Employee("John", "Winters", 2);
+
+            Console.WriteLine($"Employee 1 and Employee 3 are equal: {employee1 == employee3}");
+
+            Console.ReadLine();
+        }
+    }
+}
